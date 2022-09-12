19cb76a7add01059af6e6fef27ca45aa10560a85
wzjaroszynski <112191003+wzjaroszynski@users.noreply.github.com>
2022-09-12 01:32:15 +0000

    final

--- main.cpp
+++ main.cpp
@@ -10,6 +10,17 @@ int main() {
   
     cout << " Enter coordinates [ x1 y1] [x2 y2]: "; 
     cin >> x1 >> y1 >> x2 >> y2;
+
+    cout << "your points entered: (" << x1 << " , " << y1   << ") , (" << x2 << " , " << y2 << ")" << endl;
+   
+  if (x1 == x2) {
+        cout << "Slope Undefined (vertical)";
+        return 0;
+    }
   
-    
+  // finding the slope
+    slope = ((y2 - y1) / (x2 - x1));
+    cout << setprecision(2) << fixed;
+    cout << "the value of the slope is: "<< setw(6) << setfill('0') << slope << endl;
+
   }
