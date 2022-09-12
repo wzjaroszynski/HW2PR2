af081975f4091aa50c2db7cd146ea646eaabf8ef
wzjaroszynski <112191003+wzjaroszynski@users.noreply.github.com>
2022-09-12 01:24:45 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+  
+    double x1, x2, y1, y2;
+    double slope;
+  
+    cout << " Enter coordinates [ x1 y1] [x2 y2]: "; 
+    cin >> x1 >> y1 >> x2 >> y2;
+  
+    
+  }
